740c4ca0ace06179725a9a90ead12bbb9e707846
timkingteam <timkingteam@gmail.com>
2024-07-05 03:28:08 +0200

    argument parsing

---
+++ arguments.py
@@ -0,0 +1,27 @@
+import argparse
+
+module_exercises = {
+    'cpp00': 3,
+    'cpp01': 7,
+    'cpp02': 4,
+    'cpp03': 4,
+    'cpp04': 4,
+    'cpp05': 4,
+    'cpp06': 3,
+    'cpp07': 3,
+    'cpp08': 3,
+    'cpp09': 3
+}
+module_exercises = {key: ['ex0' + str(i) for i in range(value)]for (key, value) in module_exercises.items()}
+
+parser = argparse.ArgumentParser(description='Generate files for 42 cpp modules', epilog='Github repo: https://github.com/taaae/42_cpp_generator')
+parser.add_argument('module', choices=['cpp00', 'cpp01', 'cpp02', 'cpp03', 'cpp04', 'cpp05', 'cpp06', 'cpp07', 'cpp08', 'cpp09'], metavar='cpp_module')
+parser.add_argument('exercise', type=str, nargs='?')
+args = parser.parse_args()
+if args.exercise == None:
+    pass
+    # TODO: warning message here
+elif args.exercise not in module_exercises[args.module]:
+    parser.error(f"argument exercise: invalid choice: '{args.exercise}' (choose from {module_exercises[args.module]})")
+
+# TODO: add codestyle choice argument
